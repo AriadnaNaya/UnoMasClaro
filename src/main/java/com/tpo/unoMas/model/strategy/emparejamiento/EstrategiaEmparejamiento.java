3b50ef004ad6953d9adbd7c5c8a69598f3c41edd
Ariadna <ariadnanaya2016@gmail.com>
2025-06-11 19:11:52 -0300

    Mensajes

--- src/main/java/com/tpo/unoMas/model/strategy/emparejamiento/EstrategiaEmparejamiento.java
+++ src/main/java/com/tpo/unoMas/model/strategy/emparejamiento/EstrategiaEmparejamiento.java
@@ -5,24 +5,10 @@ import com.tpo.unoMas.model.Partido;
 
 import java.util.List;
 
-/**
- * Strategy Pattern para algoritmos de emparejamiento de jugadores
- * Encuentra jugadores potenciales para invitar cuando se crea un partido
- */
 public interface EstrategiaEmparejamiento {
     
-    /**
-     * Encuentra jugadores potenciales para invitar a un partido
-     * @param partido El partido que necesita jugadores
-     * @param jugadoresDisponibles Lista de todos los jugadores registrados
-     * @return Lista de jugadores a los que se les enviará invitación
-     */
     List<Jugador> encontrarJugadoresPotenciales(Partido partido, List<Jugador> jugadoresDisponibles);
     
-    /**
-     * Obtiene la descripción de la estrategia
-     * @return Descripción del algoritmo de emparejamiento
-     */
     String getDescripcion();
 
     boolean esCompatible(Partido partido, Jugador jugador);
